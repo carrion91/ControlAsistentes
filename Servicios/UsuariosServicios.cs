e4274e1bb5121da4f89b8abcbaf8468bd820fc7f
JesusTorresRodriguez <felipe-torres23@hotmail.com>
2020-03-27 17:32:06 -0600

    Se agrega la pagina para AdministrarUsuarios
    Se crea la pagina para administrar los usuarios

---
+++ Servicios/UsuariosServicios.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccesoDatos;
+using Entidades;
+
+namespace Servicios
+{
+	public class UsuariosServicios
+	{
+
+		#region variables globales
+		private UsuarioDatos usuarioDatos = new UsuarioDatos();
+		#endregion
+
+
+
+		#region metodos
+		/// <summary>
+		/// Jesús Torres R
+		/// 27/03/2020
+		/// Efecto: Regresa la lista de Usuarios 
+		/// Requiere: -
+		/// Modifica: -
+		/// Devuelve: Lista de Usuarios
+		/// </summary>
+		/// <returns></returns>
+		public List<Usuario> ObtenerUsuarios()
+		{
+			return usuarioDatos.ObtenerUsuarios();
+		}
+		#endregion
+	}
+}
