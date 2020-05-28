b1a48f7419a7bccfeee3f2321ce9132b08cdf2c1
lyka-guillen-a <62961193+lyka-guillen-a@users.noreply.github.com>
2020-05-28 16:34:31 -0600

    Inclusión de Proyectos al SiteMaster

--- Servicios/ProyectoServicios.cs
+++ Servicios/ProyectoServicios.cs
@@ -1,11 +1,6 @@
 ﻿using AccesoDatos;
 using Entidades;
-using Microsoft.EntityFrameworkCore.Query.Internal;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Servicios
 {
